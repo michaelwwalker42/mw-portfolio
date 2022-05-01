a6b574305b568e76dc578a6c5f2f66d24540bf15
Michael Walker <michaelwwalker42@gmail.com>
2022-04-30 19:03:28 -0700

    add more projects to work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+
 import rocks from '../assets/project-images/rocks-ropes.png';
 import song from '../assets/project-images/song-starter.png'
 import tech from '../assets/project-images/tech-blog.png'
 import weather from '../assets/project-images/weather-dashboard.png'
+
+const projects = [
+  {
+    id: 1,
+    image: rocks,
+    repo: 'https://github.com/dmknapp2385/rocks-and-ropes',
+    deployment: 'https://rocks-and-ropes.herokuapp.com/',
+    description: 'MERN Stack Landing Page'
+  },
+  {
+    id: 2,
+    image: song,
+    repo: 'https://github.com/michaelwwalker42/Song-Starter-3000',
+    deployment: 'https://song-starter-3000.herokuapp.com/',
+    description: 'Chord Progression Generator (Full Stack)'
+
+  },
+  {
+    id: 3,
+    image: tech,
+    repo: 'https://github.com/michaelwwalker42/tech-blog',
+    deployment: 'https://walker-tech-blog.herokuapp.com/',
+    description: 'CMS Style Blog'
+  },
+
+  {
+    id: 4,
+    image: weather,
+    repo: 'https://github.com/michaelwwalker42/weather-dashboard',
+    deployment: 'https://michaelwwalker42.github.io/weather-dashboard/',
+    description: 'Weather App'
+  }
+]
 
 const Work = () => {
   return (
@@ -10,34 +44,36 @@ const Work = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-[#e5dcc5]'>Work</p>
-          <p className='py-6'>Check out some of my recent work</p>
         </div>
         {/* Container */}
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+          {/* Project Cards */}
+          {projects.map(project => (
+            <div
+              style={{ backgroundImage: `url(${project.image})` }}
+              className='shadow-lg shadow-[#050D19] group container rounded-md flex justify-center items-center mx-auto content-div' key={project.id} >
 
-          <div
-            style={{ backgroundImage: `url(${rocks})` }}
-            className='shadow-lg shadow-[#050D19] group container rounded-md flex justify-center items-center mx-auto content-div'>
-
-            {/* Hover effects */}
-            <div className='opacity-0 group-hover:opacity-100'>
-              <span className='text-xl font-bold text-shadow tracking-wider'>
-                MERN Stack Application
-              </span>
-              <div className='pt-8 text-center'>
-                <a href="https://rocks-and-ropes.herokuapp.com/">
-                  <button className='w-[64px] shadow-sm shadow-[#050D19] hover:scale-110 duration-200 text-center rounded-lg  py-2 m-2 bg-[#e5dcc5] text-[#0d1d34]'>
-                    Demo
+              {/* Hover effects */}
+              <div className='opacity-0 group-hover:opacity-100'>
+                <span className='text-xl font-bold text-shadow tracking-wider'>
+                  {project.description}
+                </span>
+                <div className='pt-8 text-center'>
+                  <a href={project.deployment}>
+                    <button className='w-[64px] shadow-sm shadow-[#050D19] hover:scale-110 duration-200 text-center rounded-lg  py-2 m-2 bg-[#e5dcc5] text-[#0d1d34]'>
+                      Demo
                     </button>
-                </a>
-                <a href="https://github.com/dmknapp2385/rocks-and-ropes">
-                  <button className='w-[64px] shadow-sm shadow-[#050D19] hover:scale-110 duration-200 text-center rounded-lg  py-2 m-2 bg-[#e5dcc5] text-[#0d1d34]'>
-                    Code
+                  </a>
+                  <a href={project.repo}>
+                    <button className='w-[64px] shadow-sm shadow-[#050D19] hover:scale-110 duration-200 text-center rounded-lg  py-2 m-2 bg-[#e5dcc5] text-[#0d1d34]'>
+                      Code
                     </button>
-                </a>
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
+
         </div>
       </div>
     </div>
