9faa099be35b8f3c3cb7c150f7efbd5e693df1e0
Michael Walker <michaelwwalker42@gmail.com>
2022-05-04 14:26:37 -0700

    add icon and title

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Helmet from "react-helmet";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
@@ -8,9 +9,14 @@ import Work from "./components/Work";
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Michael Walker - Portfolio</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      <Navbar />
       <Home />
-      <About /> 
+      <About />
       <Skills />
       <Work />
       <Contact />
